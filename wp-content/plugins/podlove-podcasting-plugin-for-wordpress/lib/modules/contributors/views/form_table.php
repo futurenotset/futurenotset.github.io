2d1d17704fedbb5f238666e44cd26bb97dc74f0b
Dave Keeshan <dave.keeshan@daxzio.com>
2024-01-24 00:20:55 +0100

    More migration work

---
+++ wp-content/plugins/podlove-podcasting-plugin-for-wordpress/lib/modules/contributors/views/form_table.php
@@ -0,0 +1,9 @@
+<div id="contributors-form">
+    <table class="podlove_alternating" border="0" cellspacing="0">
+        <thead>
+            <tr>
+                <th class="podlove-avatar-column" colspand="2"></th>
+</tr>
+</thead>
+</table>
+</div>
